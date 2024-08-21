3fa1af5ee1a5ab636ca586648a2596701bbdb0f5
Seho_Si <sunnyseho01@gmail.com>
2024-08-21 21:12:58 +0900

    초기 커밋.

---
+++ backjoon_single_array/backjoon_single_array.cpp
@@ -0,0 +1,21 @@
+﻿#include<stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main() {
+	int n;
+	scanf("%d\n", &n);
+
+	int a[101];
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &a[i]);
+	}
+	int v;
+	scanf("%d", &v);
+
+	int result = 0;
+	for (int i = 0; i < n; i++) {
+		if (a[i] == v)
+			result++;
+	}
+	printf("%d", result);
+}
