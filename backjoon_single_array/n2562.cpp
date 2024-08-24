423e8050c81f5f300bf780551cb43ac3b0256d7c
Seho_Si <sunnyseho01@gmail.com>
2024-08-24 12:11:05 +0900

    배열 활용할 필요 x

---
+++ backjoon_single_array/n2562.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int max = 0; //최댓값
+    int index = 0; //몇 번째 자리인지
+    int x;
+    for (int i = 1; i <= 9; i++) {
+        scanf("%d", &x);
+        if (max < x) {
+            max = x;
+            index = i;
+        }
+    }
+    printf("%d\n%d", max, index);
+    return 0;
+}
