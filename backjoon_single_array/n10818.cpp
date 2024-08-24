f690dbda9f7a608e3ea6fb032f1426989964bfd3
Seho_Si <sunnyseho01@gmail.com>
2024-08-24 11:42:16 +0900

    변수 크기 변수 명으로도 할당 가능

---
+++ backjoon_single_array/n10818.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+	int n, min = 1000000, max = -1000000; // min, max 범위 설정 주의하기
+
+	scanf("%d", &n);
+	int num[n]; // 배열의 크기를 변수로 설정
+
+	for (int i = 0; i < n; i++) // 최소, 최대값 구하기
+	{
+		scanf("%d", &num[i]);
+		if (num[i] > max) //min, max값 최신화
+			max = num[i]; 
+		if (num[i] < min)
+			min = num[i];
+	}
+
+	printf("%d %d", min, max); //결과 출력
+
+	return 0;
+
+}
--- backjoon_single_array/n10871.cpp
+++ backjoon_single_array/n10871.cpp
@@ -1,15 +1,15 @@
-#include<stdio.h>
-#define _CRT_SECURE_NO_WARNINGS
-
-int main() {
-	int n, x;
-	scanf("%d %d", &n, &x);
-	int array[10001];
-	for (int i = 0; i < n; i++) {
-		scanf("%d", &array[i]);
-	}
-	for (int i = 0; i < n; i++) {
-		if (array[i] < x)
-			printf("%d ", array[i]);
-	}
-}
+//#include<stdio.h>
+//#define _CRT_SECURE_NO_WARNINGS
+//
+//int main() {
+//	int n, x;
+//	scanf("%d %d", &n, &x);
+//	int array[10001];
+//	for (int i = 0; i < n; i++) {
+//		scanf("%d", &array[i]);
+//	}
+//	for (int i = 0; i < n; i++) {
+//		if (array[i] < x)
+//			printf("%d ", array[i]);
+//	}
+//}
